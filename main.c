463a880f7fd1217e12332e98a355c210cea89f82
Miuywu <859@holbertonschool.com>
2019-09-25 10:45:11 -0700

    merge el_maine and main

--- main.c
+++ main.c
@@ -9,12 +9,22 @@
 int main(int ac, char **av)
 {
         int fdopen;
+	char *buffer;
+	char *tokens;
 
         if (ac != 2)
 		error_handler("use_err", "", "");
         fdopen = open(filename, O_RDWR);
         if (fdopen == -1)
                 error_handler("fopen_error" , av[1], "");
+
+	buffer = file_reader(argv[1]);
+	tokens = strtok(buffer, " \n");
+	while (tokens != NULL)
+	{
+		printf("%s\n", tokens);
+		tokens = strtok(NULL, " \n");
+	}
         /*if opcode reader returns -1
 	 error_handler("op_err", line, "", lineNumber);
 	 */
