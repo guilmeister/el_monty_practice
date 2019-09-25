74349aefdb26ddc3892deb07d1ae160c8aa9d5af
miuywu <859@holbertonschool.com>
2019-09-25 02:32:03 +0000

    removes the top node on stack

--- pop.c
+++ pop.c
@@ -1,2 +1,19 @@
 #include "monty.h"
+/**
+ * pop - deletes top node on stack
+ * @h: top of stack
+ * Return: NULL
+ */
+void pall(const stack_t *h)
+{
+	const stack_t *top = h;
+	const stack_t *topNext = h->next;
+	const stack_t *topPrev = h->prev;
 
+	/* if h == NULL */
+	/* if *h == NULL */
+	h = h->prev;
+	free(topNext);
+	free(topPrev);
+	free(top);
+}
